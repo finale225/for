f229324bc5d30d5d2506b7c5bf3a515a2e8b6efe
finale225 <finale2255@naver.com>
2021-06-22 16:16:37 +0900

    CaseA+B_2

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main() {
     cin >> t;
     for (int i=0; i<t; i++) {
         cin >> a >> b;
-        cout << "Case #" << i+1 << ": " << a+b << '\n';
+        cout << "Case #" << i+1 << ": " << a << " + " << b << " = " << a+b << '\n';
     }
 }
