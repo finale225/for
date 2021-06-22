05f29243a6ef16ee3e896cc36b94d32a5736981d
finale225 <finale2255@naver.com>
2021-06-22 15:59:40 +0900

    speedyA+B

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n, sum = 0;
+int main() {
+    int n, a, b;
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
     cin >> n;
-    for (int i=1; i<=n; i++) sum += i;
-    cout << sum;
+    for (int i=0; i<n; i++) {
+        cin >> a >> b;
+        cout << a+b << '\n';
+    }
 }
