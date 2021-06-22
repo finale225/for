a4153bfd7f6205c361f3bcf5f8d28c06b5ace958
finale225 <finale2255@naver.com>
2021-06-22 16:53:40 +0900

    printStars2

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<=i; j++) cout << '*';
+    for (int i=n; i>0; i--) {
+        for (int j=1; j<i; j++) cout << ' ';
+        for (int k=n; k>=i; k--) cout << '*';
         cout << endl;
     }
 }
