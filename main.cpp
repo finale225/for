8429ad04d54fc9079f3a662ee70addad66173daa
finale225 <finale2255@naver.com>
2021-06-22 15:44:22 +0900

    sum

--- main.cpp
+++ main.cpp
@@ -1,28 +1,9 @@
-/*#include <iostream>
-#include <vector>
-using namespace std;
-
-int main() {
-    vector<int> v;
-    int t, a, b;
-    cin >> t;
-    for (int i=0; i<t; i++){
-        cin >> a >> b;
-        v.push_back(a+b);
-    }
-    for (int i=0; i<t; i++) {
-        cout << v[i] << endl;
-    }
-}*/
-
 #include <iostream>
 using namespace std;
 
 int main(){
-    int t, a, b;
-    cin >> t ;
-    for(int i=0 ;i<t; i++){
-        cin >> a >> b ;
-        cout << a+b << endl;
-    }
+    int n, sum = 0;
+    cin >> n;
+    for (int i=1; i<=n; i++) sum += i;
+    cout << sum;
 }
